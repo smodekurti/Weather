333cc21f16413f604ae7d79f42dbe3082863b8b3
smodekurti <hivasu@gmail.com>
2014-11-28 16:11:04 -0600

    Added new content

---
+++ www/js/WeatherService.js
@@ -0,0 +1,143 @@
+var app = angular.module('weatherApp');
+app.factory('WeatherService',['$http','$q','$log',function($http,$q,$log){
+   
+    var googleURL = "http://maps.google.com/maps/api/geocode/json?address=";
+    var forecastURLBegin = "https://api.forecast.io/forecast/88c86c20c4bbc82064e39c376f56a295/";
+    var forecastURLEnd = "?callback=JSON_CALLBACK";
+   
+    var geoLocation = {};
+    WeatherResult = {};
+    
+    
+    var weekday = new Array(7);
+    weekday[0]=  "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    
+    return ({
+        findGeoLocation : findGeoLocation,
+        findWeatherByGeo: findWeatherByGeo
+    });
+    
+
+
+function findGeoLocation(zipCode){
+    var request = $http.get(googleURL+zipCode,{});
+        
+    return (request.then(handleGeoSuccess,handleGeoError));
+}
+
+    
+function findWeatherByGeo(latitude, longitude){
+    
+    var forecastURL = forecastURLBegin+latitude+","+longitude+forecastURLEnd;
+    var request     = $http.jsonp(forecastURL,{});
+    return(request.then(handleWeatherCallSuccess, handleWeatherCallError));
+}
+    
+function handleWeatherCallSuccess(response){
+    var result1 = response;
+    $log.info(result1);
+    var d = new Date();
+    WeatherResult.Status = "Success";
+    WeatherResult.CurrentDay = weekday[d.getDay()];
+    WeatherResult.CurrentWeather = 
+                        {
+                            currentTemperature : Math.round(result1.data.currently.temperature),
+                            icon : result1.data.currently.icon,
+                            iconSize : 30,
+                            summary : result1.data.currently.summary,
+                            weekSummary : result1.data.daily.summary
+                        };
+    
+    var dailyWeatherArray = new Array(7);
+
+    for(i=0;i<dailyWeatherArray.length;i++){
+
+        var dayCount = "Day"+i;
+        var day = d.getDay()+i;
+
+        if(day >= weekday.length){
+            day = day-weekday.length;
+        }
+        var dailyWeather = {"DayName" : weekday[day],
+                            "minTemperature" : Math.round(result1.data.daily.data[i].temperatureMin),
+                            "maxTemperature" : Math.round(result1.data.daily.data[i].temperatureMax),
+                            "icon" : result1.data.daily.data[i].icon,
+                            "iconSize" : 30,
+                            "summary" : result1.data.daily.data[i].summary
+                           };
+
+        dailyWeatherArray[i] = dailyWeather;   
+
+
+    } 
+
+    WeatherResult.DailyWeatherArray = dailyWeatherArray;
+   
+    return WeatherResult;
+
+}
+
+    
+
+function handleWeatherCallError(response){   
+    if (
+        ! angular.isObject( response.data ) ||
+        ! response.data.message
+        ) {
+            return( $q.reject( "An unknown error occurred." ) );
+        }
+    WeatherResult.Status = "Fail";
+    WeatherResult.CurrentDay = '';
+    WeatherResult.CurrentWeather = {};
+    WeatherResult.DailyWeatherArray = {};
+    
+    return( WeatherResult);
+}
+
+    
+    
+function handleGeoError(response){
+    if (
+        ! angular.isObject( response.data ) ||
+        ! response.data.message
+        ) {
+            return( $q.reject( "An unknown error occurred." ) );
+        }
+    
+      geoLocation.status = "Fail";
+      geoLocation.latitude = '';
+      geoLocation.longitude = '';
+      geoLocation.citylocation='';
+    
+    return( geoLocation);
+}
+
+function handleGeoSuccess(response){
+   var result = response.data;
+
+    if(result.results.length >0){
+        geoLocation.status = "Success";
+        geoLocation.latitude = result.results[0].geometry.location.lat;
+        geoLocation.longitude = result.results[0].geometry.location.lng;
+        geoLocation.citylocation=result.results[0].formatted_address;
+    }
+    else{
+      geoLocation.status = "Fail";
+      geoLocation.latitude = '';
+      geoLocation.longitude = '';
+      geoLocation.citylocation='';   
+        
+    }
+
+        
+        return geoLocation;
+}
+    
+    
+}]);
--- www/js/app.js
+++ www/js/app.js
@@ -1,92 +1,56 @@
-var app =angular.module('weatherApp',['angular-skycons']);
+var app =angular.module('weatherApp',['angular-skycons','mgcrea.ngStrap']);
 
-app.controller('WeatherController',['$scope','$log','$http','$filter',function($scope,$log,$http,$filter){
+app.controller('WeatherController',['$scope','$log','$http','$filter','WeatherService',function($scope,$log,$http,$filter,WeatherService){
     
-   
-    
-    var d = new Date();
-    var weekday = new Array(7);
-    weekday[0]=  "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
-    
-    
-    $scope.currentdDay = weekday[d.getDay()];
-    
-    $scope.zipCode = '60089';
-    $scope.latitude = '42.1295221';
-    $scope.longitude = '-87.994985';
-    $scope.citylocation='';
-    $scope.CurrentWeather={};
-    $scope.DailyWeather={};
+
+    $scope.zipCode = '';
+    $scope.WeatherResult={};
+    $scope.geoLocation = {};
     
     $scope.getWeather=function(){
         
         
-        var googleURL = "http://maps.google.com/maps/api/geocode/json?address="+$scope.zipCode;
+        $scope.WeatherResult={};
+        $scope.geoLocation = {};      
+       
+        var geoLocation = WeatherService.findGeoLocation($scope.zipCode);
         
-        $http.get(googleURL,{})
-            .success(function(result){
-                
-                if(result.results.length >0){
-                    
-                    $scope.latitude = result.results[0].geometry.location.lat;
-                    $scope.longitude = result.results[0].geometry.location.lng;
-                    $scope.citylocation=result.results[0].formatted_address;
-                    
-                    var forecastURL = "https://api.forecast.io/forecast/88c86c20c4bbc82064e39c376f56a295/"+$scope.latitude+","+$scope.longitude+"?callback=JSON_CALLBACK";
-                   $log.info(forecastURL);
-                    $http.jsonp(forecastURL,{})
-                        .then(function(result1){
-                        $log.info(result1);
-                        $log.info(result1.data.currently.summary);
-                        $scope.CurrentWeather = 
-                            {
-                                currentTemperature : Math.round(result1.data.currently.temperature),
-                                icon : result1.data.currently.icon,
-                                iconSize : 30,
-                                summary : result1.data.currently.summary,
-                                weekSummary : result1.data.daily.summary
-                            };
-                        var dailyWeatherArray = new Array(7);
-                        
-                        for(i=0;i<dailyWeatherArray.length;i++){
-                            
-                            var dayCount = "Day"+i;
-                            var day = d.getDay()+i;
-                            
-                            if(day >= weekday.length){
-                                day = day-weekday.length;
-                            }
-                            var dailyWeather = {"DayName" : weekday[day],
-                                                "minTemperature" : Math.round(result1.data.daily.data[i].temperatureMin),
-                                                "maxTemperature" : Math.round(result1.data.daily.data[i].temperatureMax),
-                                                "icon" : result1.data.daily.data[i].icon,
-                                                "iconSize" : 30,
-                                                "summary" : result1.data.daily.data[i].summary
-                                               };
-                       
-                            dailyWeatherArray[i] = dailyWeather;   
-                            
-                             
-                        }; 
-                        
-                        $scope.DailyWeatherArray = dailyWeatherArray;
-                    
-                    
-                    })
-                }
-                else{
-                    $log.error("Incorrect ZipCode");
-                }
-            })
-            .error(function(data, status){
-                $log.error(status); 
-        });
+            
+         geoLocation.then(function (geoLocation){
+            $scope.geoLocation = geoLocation 
+            if(geoLocation.status === "Success"){
+               var weatherDetails = WeatherService.findWeatherByGeo(geoLocation.latitude,geoLocation.longitude);    
+               weatherDetails
+                .then(function(WeatherResult){
+                    $scope.WeatherResult = WeatherResult;
+                });
+            }
+            else{
+                $log.error("Incorrect ZipCode");
+            }
+         });
+            
     }
                                    
 }]);
+
+
+// ng-directive to put focus on a field
+app.directive('focus',function($timeout) {
+    return {
+        scope : {
+            trigger : '@focus'
+        },
+        link : function(scope, element) {
+            scope.$watch('trigger', function(value) {
+                if (value === "true") {
+                    $timeout(function() {
+                        element[0].focus();
+                    });
+                }
+            });
+        }
+  };
+
+});
+
